976e95eafb910630993783d45e56cef917edcc60
Sindre Sorhus <sindresorhus@gmail.com>
2022-09-06 11:59:34 +0700

    Create index.js

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ export default function arrayShuffle(array) {
 	if (!Array.isArray(array)) {
 		throw new TypeError(`Expected an array, got ${typeof array}`);
 	}
-	// if array was empty return null
-	if(array.length === 0) return [];
+
+	if (array.length === 0) {
+		return [];
+	}
+
 	array = [...array];
 
 	for (let index = array.length - 1; index > 0; index--) {
