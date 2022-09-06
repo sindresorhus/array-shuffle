67b4b2ad7eca5a682f147fc9b34d2ae7a6ef5333
TrueXPixells <68606364+TrueXPixells@users.noreply.github.com>
2022-09-06 07:59:59 +0300

    Minor refactoring (#7)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ export default function arrayShuffle(array) {
 		throw new TypeError(`Expected an array, got ${typeof array}`);
 	}
 
+	if (array.length === 0) {
+		return [];
+	}
+
 	array = [...array];
 
 	for (let index = array.length - 1; index > 0; index--) {
